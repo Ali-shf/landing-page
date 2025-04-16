6b61ef7a1ea709a1f981d6b80e66071ac8160f47
Ali Shahrabi <Ali88206497@gmail.com>
2025-04-16 15:59:43 +0330

    improved hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // hamburger-menu
-const hamburgerMenu = document.querySelector("#mobile-navbar .hamburger-container");
+const hamburgerMenu = document.querySelector("#mobile-navbar > img");
 hamburgerMenu.addEventListener("click", () => {
     const hamburgerList = document.querySelector("#hamburger-list");
+    const hamburgerContaner = document.querySelector("#mobile-navbar .hamburger-container");
     hamburgerList.classList.remove("hidden");
-    hamburgerMenu.classList.toggle("active");
+    hamburgerContaner.classList.toggle("active");
 })
